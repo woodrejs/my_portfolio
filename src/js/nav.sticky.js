e4beb9c653e9923331539fa374af3287ad5f4eed
woodrejs <woodrejs@gmail.com>
2021-11-26 12:53:23 +0100

    main img update

--- src/js/nav.sticky.js
+++ src/js/nav.sticky.js
@@ -5,11 +5,13 @@ let isSticky = false;
 document.addEventListener("scroll", (e) => {
   if (window.pageYOffset > windowHeight && !isSticky) {
     isSticky = true;
+    nav.classList.add("shadow");
     nav.style.position = "fixed";
   }
 
   if (window.pageYOffset <= windowHeight && isSticky) {
     isSticky = false;
+    nav.classList.remove("shadow");
     nav.style.position = "relative";
   }
 });
